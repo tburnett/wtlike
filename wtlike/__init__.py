2add77426a5cf2c938b114b32eeb169c94282a96
Toby Burnett <tburnett@uw.edu>
2021-09-01 06:31:05 -0700

    for the collaboration meeting

--- wtlike/__init__.py
+++ wtlike/__init__.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-from .config import Config, UTC, MJD
+from .config import Config, UTC, MJD, Timer
 from .data_man import check_data
 from .sources import PointSource, FermiCatalog
 from .loglike import PoissonRep
